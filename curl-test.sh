21f5a3aba0ef36e2342e406af3b4476e285dfbd8
Alper <alper.makcan@gmail.com>
2026-02-24 13:28:49 +0300

    Create a new curl script

---
+++ curl-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# For direct messages
+curl -X POST https://${{ vars.ZULIP_ORGANIZATION_NAME }}/api/v1/messages \
+    -u ${{ secrets.USER_EMAIL }}:${{ secrets.USER_API_KEY }} \
+    --data-urlencode type=direct \
+    --data-urlencode `to=[${{ secrets.MESSAGE_RECEIVER_UER_ID }}]` \
+    --data-urlencode 'content=test test testttttttttttt'
