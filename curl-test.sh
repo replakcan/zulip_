aaa7bd4f4b95fc4077eca78e37b57037fd6caf47
Alper <alper.makcan@gmail.com>
2026-02-24 14:34:10 +0300

    add date info for dm

--- curl-test.sh
+++ curl-test.sh
@@ -4,4 +4,4 @@ curl -X POST "https://${ZULIP_ORGANIZATION_NAME}.zulipchat.com/api/v1/messages" 
     -u "${USER_EMAIL}:${USER_API_KEY}" \
     --data-urlencode "type=direct" \
     --data-urlencode "to=[${MESSAGE_RECEIVER_USER_ID}]" \
-    --data-urlencode "content=test test testttttttttttt"
+    --data-urlencode "content=test test `date`"
