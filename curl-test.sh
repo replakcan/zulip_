2ecf2b5064cbc71f17baa221e09415b70b2685bc
Alper <alper.makcan@gmail.com>
2026-02-24 14:04:29 +0300

    test

--- curl-test.sh
+++ curl-test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# curl -X POST "https://${ZULIP_ORGANIZATION_NAME}.zulipchat.com/api/v1/messages" \
-#    -u "${USER_EMAIL}:${USER_API_KEY}" \
-#    --data-urlencode "type=direct" \
-#    --data-urlencode "to=[${MESSAGE_RECEIVER_USER_ID}]" \
-#    --data-urlencode "content=test test testttttttttttt"
-
-echo "${ZULIP_ORGANIZATION_NAME}"
-echo "${USER_EMAIL}"
+curl -X POST "https://${ZULIP_ORGANIZATION_NAME}.zulipchat.com/api/v1/messages" \
+    -u "${USER_EMAIL}:${USER_API_KEY}" \
+    --data-urlencode "type=direct" \
+    --data-urlencode "to=[${MESSAGE_RECEIVER_USER_ID}]" \
+    --data-urlencode "content=test test testttttttttttt"
