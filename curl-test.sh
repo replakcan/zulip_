2fb5e988599488180b5398d8da894b1cce38fb34
Alper <alper.makcan@gmail.com>
2026-02-24 13:30:48 +0300

    Fix typo

--- curl-test.sh
+++ curl-test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# For direct messages
 curl -X POST https://${{ vars.ZULIP_ORGANIZATION_NAME }}/api/v1/messages \
     -u ${{ secrets.USER_EMAIL }}:${{ secrets.USER_API_KEY }} \
-    --data-urlencode type=direct \
-    --data-urlencode `to=[${{ secrets.MESSAGE_RECEIVER_UER_ID }}]` \
-    --data-urlencode 'content=test test testttttttttttt'
+    --data-urlencode "type=direct" \
+    --data-urlencode "to=[${{ secrets.MESSAGE_RECEIVER_USER_ID }}]" \
+    --data-urlencode "content=test test testttttttttttt"
