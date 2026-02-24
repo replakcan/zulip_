fe96517a25b01ddd31aecd4ad46decc090aecc17
Alper <alper.makcan@gmail.com>
2026-02-24 14:08:07 +0300

    test

--- curl-test.sh
+++ curl-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST "https://${ZULIP_ORGANIZATION_NAME}.zulipchat.com/api/v1/messages" \
+curl -X POST "https://bilgisayarkavramlari.zulipchat.com/api/v1/messages" \
     -u "${USER_EMAIL}:${USER_API_KEY}" \
     --data-urlencode "type=direct" \
     --data-urlencode "to=[${MESSAGE_RECEIVER_USER_ID}]" \
