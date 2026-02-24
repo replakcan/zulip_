5d7128e4bc9e2bc43045ac3a2b9cce8b5245cd47
Alper <alper.makcan@gmail.com>
2026-02-24 13:35:13 +0300

    Adds double quotes around the vars && secrets

--- curl-test.sh
+++ curl-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -X POST https://${{ vars.ZULIP_ORGANIZATION_NAME }}/api/v1/messages \
-    -u ${{ secrets.USER_EMAIL }}:${{ secrets.USER_API_KEY }} \
+curl -X POST "https://${{ vars.ZULIP_ORGANIZATION_NAME }}/api/v1/messages" \
+    -u "${{ secrets.USER_EMAIL }}:${{ secrets.USER_API_KEY }}" \
     --data-urlencode "type=direct" \
     --data-urlencode "to=[${{ secrets.MESSAGE_RECEIVER_USER_ID }}]" \
     --data-urlencode "content=test test testttttttttttt"
