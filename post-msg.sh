c0580563c9b4eabdd94733d144659505147027ae
Alper <alper.makcan@gmail.com>
2026-02-25 13:50:20 +0300

    Updates post-msg script structure

---
+++ post-msg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+URL="https://${ORG_NAME}.zulipchat.com/api/v1/messages"
+
+curl -X POST URL \
+    -u "${SENDER_EMAIL_ADDRESS}:${SENDER_API_KEY}" \
+    --data-urlencode "type=direct" \
+    --data-urlencode "to=[${RECEIVER_ID}]" \
+    --data-urlencode "content=test test `date`"
