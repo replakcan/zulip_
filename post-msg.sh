b896e8f6c7ea05118b85a06ec4d441a788610db0
Alper <alper.makcan@gmail.com>
2026-02-25 13:52:55 +0300

    Gets URL dynamically

--- post-msg.sh
+++ post-msg.sh
@@ -2,7 +2,7 @@
 
 URL="https://${ORG_NAME}.zulipchat.com/api/v1/messages"
 
-curl -X POST URL \
+curl -X POST ${URL} \
     -u "${SENDER_EMAIL_ADDRESS}:${SENDER_API_KEY}" \
     --data-urlencode "type=direct" \
     --data-urlencode "to=[${RECEIVER_ID}]" \
