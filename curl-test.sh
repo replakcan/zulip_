22f70b2be47943fb59d05186afc30d17b2bdf70e
Alper <alper.makcan@gmail.com>
2026-02-25 13:48:22 +0300

    Updates script file names

--- curl-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST "https://${ZULIP_ORGANIZATION_NAME}.zulipchat.com/api/v1/messages" \
-    -u "${USER_EMAIL}:${USER_API_KEY}" \
-    --data-urlencode "type=direct" \
-    --data-urlencode "to=[${MESSAGE_RECEIVER_USER_ID}]" \
-    --data-urlencode "content=test test `date`"
--- my-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Hello mello"
-
-echo "Today is " `date`
